c01809eceaf2a2881e30dbc2476d2b0b39f37529
unknown <katungaarmand1@gmail.com>
2022-03-23 20:37:50 +0000

    setting up the CRUD system and connection to the DB

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('katunga')
+const connectDB = require('./src/db/connnect')
+const express = require('express')
+const app = express()
+const routes = require('./src/routers/index')
+require('dotenv').config()
+
+
+//middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+//route
+
+app.use('/api/v1/index',routes)
+
+const port = 3000
+ 
+const start = async ()=>{
+    try {
+
+        await connectDB(process.env.MONGO_URL)
+    }catch(error) {
+        console.log(error)
+    }
+}
+start()
+
+app.listen(port,console.log(`the app is listening on port ${port}.....`))
